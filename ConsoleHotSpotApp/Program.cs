e94d00e66440bd05e8a2206f686d831adc29c1cb
nkildibekov <nickkildibekov@gmail.com>
2019-03-15 11:38:50 +0200

    - minor fixes

--- ConsoleHotSpotApp/Program.cs
+++ ConsoleHotSpotApp/Program.cs
@@ -4,11 +4,14 @@ namespace ConsoleHotSpotApp
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            const int period = 1;
+            Console.Write("Enter a correct ( >= 0 )NUMBER  of days for analyst period :  ");
+            var period = Convert.ToInt32(Console.ReadLine());
+            if (period <= 0) Main();
             var modifiedOnOrAfter = DateTime.Now.AddDays(-period);
             Methods.A(modifiedOnOrAfter);
         }
     }
 }
+
