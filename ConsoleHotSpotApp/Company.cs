8e9c68c54af08453056144fda5d2a61d69b72dbb
nkildibekov <nickkildibekov@gmail.com>
2019-03-15 11:26:01 +0200

    - firet and last commit

---
+++ ConsoleHotSpotApp/Company.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace HotSpotUserInfo.Models
+{
+    public class Company
+    {
+        public int? Id { get; set; }
+        public string Company_Name { get; set; }
+        public string Company_WebSite { get; set; }
+        public string Company_City { get; set; }
+        public string Company_State { get; set; }
+        public int Company_ZipCode { get; set; }
+        public string Company_Phone { get; set; }
+    }
+}
---
+++ ConsoleHotSpotApp/Contact.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace HotSpotUserInfo.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LifeCycleStage { get; set; }
+        public Company Company { get; set; }
+        public Contact()
+        {
+            Company = new Company();
+        }
+    }
+}
